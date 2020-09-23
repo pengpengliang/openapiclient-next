687d5c83351a2491815db5b62282b3872dc803d7
liangzhanpeng <zhanpengl@chinadci.com>
2020-09-23 13:59:00 +0800

    首页框架

--- src/views/index/Index.vue
+++ src/views/index/Index.vue
@@ -13,19 +13,7 @@
         v-model:selectedKeys="selectedKeys"
         v-model:openKeys="openKeys"
       >
-        <a-sub-menu key="sub1">
-          <template v-slot:title>
-            <span>
-              <user-outlined />
-              <span>Navigation One</span>
-            </span>
-          </template>
-          <a-menu-item key="5">Option 5</a-menu-item>
-          <a-menu-item key="6">Option 6</a-menu-item>
-          <a-menu-item key="7">Option 7</a-menu-item>
-          <a-menu-item key="8">Option 8</a-menu-item>
-        </a-sub-menu>
-        <a-sub-menu key="sub2">
+       <a-sub-menu key="sub1">
           <template v-slot:title>
             <span>
               <user-outlined />
@@ -37,6 +25,19 @@
           <a-menu-item key="3">Option 3</a-menu-item>
           <a-menu-item key="4">Option 4</a-menu-item>
         </a-sub-menu>
+        <a-sub-menu key="sub2">
+          <template v-slot:title>
+            <span>
+              <user-outlined />
+              <span>Navigation Two</span>
+            </span>
+          </template>
+          <a-menu-item key="5">Option 5</a-menu-item>
+          <a-menu-item key="6">Option 6</a-menu-item>
+          <a-menu-item key="7">Option 7</a-menu-item>
+          <a-menu-item key="8">Option 8</a-menu-item>
+        </a-sub-menu>
+       
       </a-menu>
     </a-layout-sider>
     <a-layout>
